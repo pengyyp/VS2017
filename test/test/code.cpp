147b78029a8e9c9ba8deeafff2118536befd1538
pengyyp <819394097@qq.com>
2020-08-02 11:06:48 +0800

    第二次更改

--- test/test/code.cpp
+++ test/test/code.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 int main() {
 	cout << "myhub²âÊÔ"<<endl;
-	Sleep(2000);
+	int a[3][2] = {(0,1),(2,3),(4,5)};
+	int* p = a[0];
+	printf("%d", p[0]);
+	system("pause");
 	return 1;
 }
